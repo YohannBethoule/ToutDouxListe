29428f9256fbb9959236e1593c552da04ee4b7f1
Arkandros <yoyoteva@gmail.com>
2017-11-16 17:01:59 +0100

    Début du site html/css

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arkandros
+ * Date: 16/11/17
+ * Time: 00:27
+ */
+?>
+
+<html>
+<?php require_once ('vues/head.html');?>
+
+<body>
+<div class="container-fluid">
+
+    <div class="row">
+        <nav class="col-lg-2">
+            Menu
+        </nav>
+        <section class="col-lg-10">
+            Section
+        </section>
+    </div>
+
+
+
+</div>
+
+</body>
+
+</html>
+
